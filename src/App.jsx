385229ed2e4540d2e21e5e5ad136456423c7e72c
Marta Vatamaniuk <vatamanukmarta@gmail.com>
2025-10-01 14:13:40 +0300

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList';
 
 export const App = () => (
-  <><div className="page">
+  <div className="page">
     <div className="page-content">
       <MovieList movies={moviesFromServer} />
-  </div><div className="sidebar" data-cy="Sidebar">
+    </div>
+    <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
     </div>
-  </div></>
+  </div>
 );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,8 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      < MovieCard movie={movie}/>
+      <MovieCard movie={movie} />
     ))}
   </div>
 );
+
